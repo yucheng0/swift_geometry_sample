1cf3b2f8076645ba97428f4147c5f8b5c21876b0
yuchengtseng <yucheng0939@gmail.com>
2020-12-08 13:10:48 +0800

    Initial Commit

---
+++ ex5/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ex5
+//
+//  Created by yuchengtseng on 2020/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
